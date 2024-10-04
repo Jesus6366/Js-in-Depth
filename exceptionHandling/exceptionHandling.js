04431409fd67914a295cfd8bc302fbe205ca0e6f
unknown <polibos6366@gmail.com>
2024-10-04 17:33:52 -0600

    try-catch

---
+++ exceptionHandling/exceptionHandling.js
@@ -0,0 +1,15 @@
+// throw "There was a bad thing that happened!";
+
+// console.log("hello");
+
+try {
+  const message = "hello there";
+  console.log(message);
+
+  throw "Boomn!";
+} catch (error) {
+  console.error(error);
+  console.error("caught!");
+}
+
+console.log("HI?");
